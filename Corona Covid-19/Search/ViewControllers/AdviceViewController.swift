3d730fe4824ce29299fb4863b810ad8d92acf962
emil vaklinov <emil_vaklinov@yahoo.com>
2020-04-09 10:46:25 +0100

    Files transferred from development branch.

---
+++ Corona Covid-19/Search/ViewControllers/AdviceViewController.swift
@@ -0,0 +1,30 @@
+//
+//  AdviceViewController.swift
+//  Corona Covid-19
+//
+//  Created by Emil Vaklinov on 07/04/2020.
+//  Copyright © 2020 Majdi Felah. All rights reserved.
+//
+
+import UIKit
+
+class AdviceViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
