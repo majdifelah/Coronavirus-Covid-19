cb7cfc29cf196bd96d17d29a3a86b2f8deb9a948
majdifelah <majdifelah@live.fr>
2020-04-11 10:29:56 +0100

    add new featues

---
+++ Corona Covid-19/Scenes/Live Check/View Controllers/LiveCheckViewController.swift
@@ -0,0 +1,19 @@
+//
+//  LiveCheckViewController.swift
+//  Corona Covid-19
+//
+//  Created by Majdi Felah on 11/04/2020.
+//  Copyright © 2020 Majdi Felah. All rights reserved.
+//
+
+import UIKit
+
+class LiveCheckViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        
+    }
+}
---
+++ Corona Covid-19/Scenes/Test Booking/View Controllers/TestBookingViewController.swift
@@ -0,0 +1,19 @@
+//
+//  TestBookingViewController.swift
+//  Corona Covid-19
+//
+//  Created by Majdi Felah on 11/04/2020.
+//  Copyright © 2020 Majdi Felah. All rights reserved.
+//
+
+import UIKit
+
+class TestBookingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        
+    }
+}
